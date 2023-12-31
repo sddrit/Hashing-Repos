00627007d01077544a99bae8bc6dd064c10ebf75
sddrit <81216822+sddrit@users.noreply.github.com>
2024-01-01 02:57:47 +0530

    Add files via upload

--- md5.h
+++ md5.h
@@ -1,78 +1,199 @@
-// //////////////////////////////////////////////////////////
-// md5.h
-// Copyright (c) 2014 Stephan Brumme. All rights reserved.
-// see http://create.stephan-brumme.com/disclaimer.html
-//
+/*********************************************
+ * Filename: md5uwe.c
+ * Author:
+ * Date: 21-07-2018
+ * Desc: This is md5 implementation for ISS
+ *
+ * Compile with: gcc -o md5uwe md5uwe.c
+ * Run:  ./md5uwe
+ * Source: https://en.wikipedia.org/wiki/MD5
+ ********************************************/
 
-#pragma once
-
-//#include "hash.h"
-#include <string>
-
-// define fixed size integer types
-#ifdef _MSC_VER
-// Windows
-typedef unsigned __int8  uint8_t;
-typedef unsigned __int32 uint32_t;
-typedef unsigned __int64 uint64_t;
-#else
-// GCC
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+
+
+// left-rotate function definition
+#define LEFTROTATE(x, c) (((x) << (c)) | ((x) >> (32 - (c))))
+
+// These vars will contain the hash. They are accessed by all the function as global variables
+uint32_t h0, h1, h2, h3;
+// the code will be h0[0], h0[1], h0[2], h0[3], h1[0],........h3[3]
+// IN THIS ORDER so every number should be reversed when printing (low byte first to the left)
+
+// MD5 algorithm main function
+void md5(uint8_t *initial_msg, size_t initial_len) {
+
+    // Message
+    uint8_t *msg = NULL;
+
+    // Note: All variables are unsigned 32 bit and wrap modulo 2^32 when calculating
+
+    // r specifies the per-round shift amounts
+
+    uint32_t r[] = {7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+                    5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
+                    4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+                    6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21};
+
+    // Use binary integer part of the sines of integers (in radians) as constants
+
+	// Initialize variables:
+    uint32_t k[] = {
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
+
+    h0 = 0x67452301;
+    h1 = 0xefcdab89;
+    h2 = 0x98badcfe;
+    h3 = 0x10325476;
+
+    // Pre-processing: adding a single 1 bit
+    // append "1" bit to message
+    /* Notice: the input bytes are considered as bits strings,
+       where the first bit is the most significant bit of the byte.*/
+
+    // Pre-processing: padding with zeros
+    // append "0" bit until message length in bit Ã¢â€°Â¡ 448 (mod 512)
+    // append length mod (2 pow 64) to message
+
+    int new_len;
+    for(new_len = initial_len*8 + 1; new_len%512!=448; new_len++);
+    new_len /= 8;
+
+    msg = (uint8_t*)calloc(new_len + 64, 1);
+
+    // also appends "0" bits
+    memcpy(msg, initial_msg, initial_len);
+    msg[initial_len] = 128; // write the "1" bit
+
+    uint32_t bits_len = 8*initial_len; // note, we append the len
+    memcpy(msg + new_len, &bits_len, 4);           // in bits at the end of the buffer
+
+    // Process the message in successive 512-bit chunks:
+    //for each 512-bit chunk of message:
+    int offset;
+    for(offset=0; offset<new_len; offset += (512/8)) {
+
+    // break chunk into sixteen 32-bit words w[j], 0 Ã¢â€°Â¤ j Ã¢â€°Â¤ 15
+        uint32_t *w = (uint32_t *) (msg + offset);
+
+#ifdef DEBUG
+        printf("offset: %d %x\n", offset, offset);
+        int j;
+        for(j =0; j < 64; j++) printf("%x ", ((uint8_t *) w)[j]);
+        puts("");
+#endif
+
+     // Initialize hash value for this chunk:
+        uint32_t a = h0;
+        uint32_t b = h1;
+        uint32_t c = h2;
+        uint32_t d = h3;
+
+     // Main loop:
+        uint32_t i;
+        for(i = 0; i<64; i++) {
+
+#ifdef ROUNDS
+            uint8_t *p;
+            printf("%i: ", i);
+            p=(uint8_t *)&a;
+            printf("%2.2x%2.2x%2.2x%2.2x ", p[0], p[1], p[2], p[3], a);
+
+            p=(uint8_t *)&b;
+            printf("%2.2x%2.2x%2.2x%2.2x ", p[0], p[1], p[2], p[3], b);
+
+            p=(uint8_t *)&c;
+            printf("%2.2x%2.2x%2.2x%2.2x ", p[0], p[1], p[2], p[3], c);
+
+            p=(uint8_t *)&d;
+            printf("%2.2x%2.2x%2.2x%2.2x", p[0], p[1], p[2], p[3], d);
+            puts("");
 #endif
 
 
-/// compute MD5 hash
-/** Usage:
-    MD5 md5;
-    std::string myHash  = md5("Hello World");     // std::string
-    std::string myHash2 = md5("How are you", 11); // arbitrary data, 11 bytes
+            uint32_t f, g;
 
-    // or in a streaming fashion:
+             if (i < 16) {
+                f = (b & c) | ((~b) & d);
+                g = i;
+            } else if (i < 32) {
+                f = (d & b) | ((~d) & c);
+                g = (5*i + 1) % 16;
+            } else if (i < 48) {
+                f = b ^ c ^ d;
+                g = (3*i + 5) % 16;
+            } else {
+                f = c ^ (b | (~d));
+                g = (7*i) % 16;
+            }
 
-    MD5 md5;
-    while (more data available)
-      md5.add(pointer to fresh data, number of new bytes);
-    std::string myHash3 = md5.getHash();
-  */
-class MD5 //: public Hash
-{
-public:
-  /// split into 64 byte blocks (=> 512 bits), hash is 16 bytes long
-  enum { BlockSize = 512 / 8, HashBytes = 16 };
+#ifdef ROUNDS
+            printf("f=%x g=%d w[g]=%x\n", f, g, w[g]);
+#endif
+            uint32_t temp = d;
+            d = c;
+            c = b;
+            //printf("rotateLeft(%x + %x + %x + %x, %d)\n", a, f, k[i], w[g], r[i]);
+            b = b + LEFTROTATE((a + f + k[i] + w[g]), r[i]);
+            a = temp;
 
-  /// same as reset()
-  MD5();
 
-  /// compute MD5 of a memory block
-  std::string operator()(const void* data, size_t numBytes);
-  /// compute MD5 of a string, excluding final zero
-  std::string operator()(const std::string& text);
 
-  /// add arbitrary number of bytes
-  void add(const void* data, size_t numBytes);
+        }
 
-  /// return latest hash as 32 hex characters
-  std::string getHash();
-  /// return latest hash as bytes
-  void        getHash(unsigned char buffer[HashBytes]);
+        // Add this chunk's hash to result so far:
 
-  /// restart
-  void reset();
+        h0 += a;
+        h1 += b;
+        h2 += c;
+        h3 += d;
 
-private:
-  /// process 64 bytes
-  void processBlock(const void* data);
-  /// process everything left in the internal buffer
-  void processBuffer();
+    }
 
-  /// size of processed data in bytes
-  uint64_t m_numBytes;
-  /// valid bytes in m_buffer
-  size_t   m_bufferSize;
-  /// bytes not processed yet
-  uint8_t  m_buffer[BlockSize];
+    // cleanup
+    free(msg);
 
-  enum { HashValues = HashBytes / 4 };
-  /// hash, stored as integers
-  uint32_t m_hash[HashValues];
-};
+}
+
+void calculateMd5(const char *input, uint8_t *result) {
+    size_t len = strlen(input);
+    md5((uint8_t *)input, len);
+
+    uint8_t *p = result;
+
+    // Store each byte of the hash in the result buffer
+    for (int i = 0; i <= 3; ++i) {
+        *(p++) = ((uint8_t *)&h0)[i];
+    }
+
+    for (int i = 0; i <= 3; ++i) {
+        *(p++) = ((uint8_t *)&h1)[i];
+    }
+
+    for (int i = 0; i <= 3; ++i) {
+        *(p++) = ((uint8_t *)&h2)[i];
+    }
+
+    for (int i = 0; i <= 3; ++i) {
+        *(p++) = ((uint8_t *)&h3)[i];
+    }
+}
+
